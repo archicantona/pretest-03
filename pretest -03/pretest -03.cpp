f9d19c14428b3289ee659a3d1e802ec9ec6e74d9
archicantona <48450140+archicantona@users.noreply.github.com>
2019-03-13 10:51:53 +0700

    Add files via upload

---
+++ pretest -03/pretest -03.cpp
@@ -0,0 +1,25 @@
+/*
+Nama 		 = Archi Cantona
+NPM  		 = 140810180050
+Tanggal Buat = 13 Maret 2019
+Deskripsi	 = membuat struct mahasiswa
+*/
+
+#include <iostream>
+using namespace std;
+
+struct r_mhs{
+	char NPM[14]
+	char Nama[40]
+	float ipk;
+};
+
+tydef r_mhslarikMhs[30];
+void banyakdata(int&n);
+void inputMhs(larikMhs mhs,int n);
+void cetakMhs(larikMhs mhs,int n);
+void sortnama(larikMhs mhs,int n);
+
+int main(){
+	
+}
